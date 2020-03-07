6c43d4819c636db485171708ed3a97f46271d62f
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2020-03-07 14:20:35 +0200

    little change of the title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class PersonalExpenses extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Daily expenses',
+      title: 'Daily Expenses',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Trade'),
       home: HomePage(),
     );
